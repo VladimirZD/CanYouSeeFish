ed9bd6b6032c6f033931cbaf4586dab42cd94eea
Vladimir Mašala <vladimir.masala@gmail.com>
2018-10-02 17:37:31 +0200

    First Working version...

--- extract_frames.py
+++ extract_frames.py
@@ -5,7 +5,7 @@
 # So i don't have to watch 3hours of video to see if there was any fish  Or what kind of bottom is there :)
 # For extraction of images i am using ffmpeg  https://www.ffmpeg.org/
 #D:\Development\WaterWolf\Video\ffmpeg-20180928-179ed2d-win64-static\bin\ffmpeg -i D:\Development\WaterWolf\Video\REC00332.MOV -vf fps=1/6 D:\Development\WaterWolf\Video\images\image_g%d.jpg
-#
+#TODO instructions for copying trained model...
 # ==============================================================================
 
 
@@ -15,12 +15,14 @@ import sys
 import os.path
 import subprocess
 import shlex
+import re
+
 
 
 def ensure_path_exists(path):
   """Check if file/folder exists and create if not"""
   if not os.path.exists(path):
-        os.mkdir(destination_folder)
+        os.mkdir(path)
 
 def check_if_path_exists(path):
   """Check if file/folder exists"""
@@ -40,7 +42,67 @@ def get_files_in_folder(path,extensions):
   except OSError:
       print('Cannot access ' + path +'. Probably a permissions error')
   return files
- 
+
+def extract_frames ():
+  """Extract frames from video"""
+  files=[]
+  if (not video_path=="") and os.path.splitext(video_path)[1].lower() in extensions:
+    files.append (video_path) 
+  if not video_folder=="":
+    files = files+get_files_in_folder(video_folder,extensions)
+  cnt =len(files)
+  i=0
+  for file in files:
+    i+=1
+    print ('Processing file %s (file %d of %d) please wait.' % (file,i,cnt))
+    ffmpeg_exe =os.path.join(ffmpeg_folder, "bin\\ffmpeg.exe")
+    filename_no_extension=os.path.splitext(os.path.split(file)[1])[0]
+    cmd = "%s -i %s -vf fps=1/%d %s\\%s_%%d.jpg" % (ffmpeg_exe,file,sampling_rate,destination_folder,filename_no_extension)
+    #print (cmd)
+    p = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE,universal_newlines=True)
+    out, err = p.communicate()
+    retCode = p.returncode
+    if not retCode==0:
+      print ("ERROR happened aborting!!")
+      print (err)
+      break
+
+def label_frames (): 
+  exts = ['.jpg']
+  files = get_files_in_folder(destination_folder,exts)
+  cnt =len(files)
+  i=0
+  for file in files:
+    i+=1
+    print ('Processing file %s (file %d of %d) please wait.' % (file,i,cnt))
+    scriptPath = os.path.join(tensorflow_folder,"scripts\\label_image.py")
+    graphPath =  os.path.join(tensorflow_folder,"tf_files\\retrained_graph.pb")
+    labelsPath = os.path.join(tensorflow_folder,"tf_files\\retrained_labels.txt")
+    cmd="python %s --graph=%s --labels=%s --image=%s" %(scriptPath,graphPath,labelsPath,file)
+    p = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE,universal_newlines=True)
+    out, err = p.communicate()
+    retCode = p.returncode
+    if not retCode==0:
+      print ("ERROR happened aborting!!")
+      print (err)
+      break
+    lines = out.split("\n")
+    labels = lines[3:len(lines)-1]
+    #labels.sort(key=lambda x: re.search('\((.+?)\)',x).group(1),reverse=True)
+    labels.sort(key=lambda x: get_score_from_item(x),reverse=True)
+    
+    category=labels[0].split(' ')[0]
+    score=labels[0].split(' ')[1]
+    filename=os.path.split(file)[1]
+    print ("\tImage %s is in category %s %s. Copying to %s folder." % (filename,category.upper(),score,category))
+    destination = os.path.join(destination_folder,category)
+    ensure_path_exists(destination)
+    newFileName = os.path.join(destination,filename)
+    os.rename(file, newFileName)
+
+def get_score_from_item(item):
+  return re.search('\((.+?)\)',item).group(1)
+  
 
 if __name__ == "__main__":
   ffmpeg_folder = ""
@@ -55,7 +117,8 @@ if __name__ == "__main__":
   parser.add_argument("--video_folder",  type=str, required=False, help="Folder to process, all videos in folder will be processed, subfolders ignored")
   parser.add_argument("--destination_folder",  type=str ,required=True, help="Folder in which images will be saved")
   parser.add_argument("--sampling_rate",  type=int, choices=range(1, 31), required=True, help="Sampling rate, --sampling_rate=6 means 1 image every 6 seconds of movie")
-  
+  parser.add_argument("--tensorflow_folder",  type=str, required=False, help="Folder where tensorflow was cloned and model trained")
+ 
   
   args = parser.parse_args()
 
@@ -69,6 +132,8 @@ if __name__ == "__main__":
     destination_folder = args.destination_folder    
   if args.sampling_rate:
     sampling_rate = args.sampling_rate  
+  if args.tensorflow_folder:
+    tensorflow_folder = args.tensorflow_folder
   
   check_if_path_exists(ffmpeg_folder)
   if not (video_path ==""):
@@ -76,35 +141,16 @@ if __name__ == "__main__":
   if not (video_folder ==""):
     check_if_path_exists(video_folder)
   ensure_path_exists(destination_folder)
+  check_if_path_exists(tensorflow_folder)
   
-  files=[]
+  #extract_frames()
+  label_frames()
+
   
-  #if ((".MOV").lower()) in extensions:
-  #  print ("tu sam")
-  if (not video_path=="") and os.path.splitext(video_path)[1].lower() in extensions:
-    files.append (video_path) 
-  if not video_folder=="":
-    files = files+get_files_in_folder(video_folder,extensions)
-  cnt =len(files)
-  i=0
-  for file in files:
-    i+=1
-    print ('Processing file %s (file %d of %d) please wait.' % (file,i,cnt))
-    ffmpeg_exe =os.path.join(ffmpeg_folder, "bin\\ffmpeg.exe")
-    filename_no_extension=os.path.splitext(os.path.split(file)[1])[0]
-    cmd = "%s -i %s -vf fps=1/%d %s\\%s_%%d.jpg" % (ffmpeg_exe,file,sampling_rate,destination_folder,filename_no_extension)
-    #print (cmd)
-    p = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    out, err = p.communicate()
-    retCode = p.returncode
-    if not retCode==0:
-      print ("ERROR happened aborting!!")
-      print (err)
-      break
-
-
-
-
-#python -m water_wolf_scripts.extract_frames --ffmpeg_folder=D:\Development\WaterWolf\Video\ffmpeg-20180928-179ed2d-win64-static --video_path=c:\ --video_folder=D:\Development\WaterWolf\Video --destination_folder=d:\vlado --sampling_rate=6
+  
+  
+  
+  #python -m extract_frames --ffmpeg_folder=D:\Utils\ffmpeg-4.0.2-win64-static --video_path=c:\ --video_folder=D:\Development\CanYouSeeFish\Video --destination_folder=D:\Development\CanYouSeeFish\Extracted_frames --sampling_rate=6 --tensorflow_folder=D:\Development\tensorflow-for-poets-2
+  #python -m water_wolf_scripts.extract_frames --ffmpeg_folder=D:\Utils\ffmpeg-4.0.2-win64-static\bin --video_path=c:\ --video_folder=D:\Development\CanYouSeeFish\Video --destination_folder=D:\Development\CanYouSeeFish\Extracted frames --sampling_rate=6
   
 
